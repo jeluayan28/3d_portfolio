74ef7f02565edfae392b14a41b1d659a475a7ef8
unknown <uayanjelli@gmail.com>
2024-10-16 23:15:34 +0800

    huhuhu pls work

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,3 +1,4 @@
+import { memo } from "react";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
@@ -7,11 +8,13 @@ const Tech = () => {
     <div className='flex flex-row flex-wrap justify-center gap-10'>
       {technologies.map((technology) => (
         <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
+          <MemoizedBallCanvas icon={technology.icon} />
         </div>
       ))}
     </div>
   );
 };
+
+const MemoizedBallCanvas = memo(BallCanvas);
 
 export default SectionWrapper(Tech, "");
--- src/constants/index.js
+++ src/constants/index.js
@@ -3,13 +3,6 @@ import {
     backend,
     creator,
     web,
-    javascript,
-    typescript,
-    html,
-    css,
-    reactjs,
-    redux,
-    tailwind,
     nodejs,
     mongodb,
     git,
@@ -60,34 +53,6 @@ import {
   ];
   
   const technologies = [
-    {
-      name: "HTML 5",
-      icon: html,
-    },
-    {
-      name: "CSS 3",
-      icon: css,
-    },
-    {
-      name: "JavaScript",
-      icon: javascript,
-    },
-    {
-      name: "TypeScript",
-      icon: typescript,
-    },
-    {
-      name: "React JS",
-      icon: reactjs,
-    },
-    {
-      name: "Redux Toolkit",
-      icon: redux,
-    },
-    {
-      name: "Tailwind CSS",
-      icon: tailwind,
-    },
     {
       name: "Node JS",
       icon: nodejs,
